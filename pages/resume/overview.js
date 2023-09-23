913ab92bfd3cf6d95091b4f27e26a0cc6a6c7487
fionatagious <ftang@codeforamerica.org>
2023-09-23 12:06:01 -0500

    email address href styles

--- pages/resume/overview.js
+++ pages/resume/overview.js
@@ -12,7 +12,7 @@ export default function Overview() {
           👋🏽&nbsp;&nbsp;I&apos;m a collaborative and committed full-stack software engineer passionate about using my skills to to equip organizations with the technology they need to better serve their communities. My work experiences span non-profits, government consulting firms, and mid- to late-stage startups.
         </Typography>
         <Typography className={utils.descriptionText}>
-          📫&nbsp;&nbsp;I&apos;m excited to collaborate on civic tech and sustainability. Write to me at <a href="mailto: fionatang@alum.berkeley.edu">fionatang@alum.berkeley.edu</a>.
+          📫&nbsp;&nbsp;I&apos;m excited to collaborate on civic tech and sustainability. Write to me at <a href="mailto: fionatang@alum.berkeley.edu" className={utils.email}>fionatang@alum.berkeley.edu</a>.
         </Typography>
       </CardContent>
     </Card>
