08fc954fdc1d83928ec1bf1140652e618c297313
fionatagious <ftang@codeforamerica.org>
2023-09-23 11:05:05 -0500

    padding

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -22,26 +22,25 @@ import utils from '/styles/utils.module.css'
 export default function Resume() {
   return (
     <>
-      {/* <Container maxWidth={false} sx={{ position: 'relative', minHeight: '90vh', background: 'linear-gradient(155deg, #7DCFB6 30%, #7DCFB6 30%, #00B2CA 40%, #7E6377 80%, #4B3B47 100%)' }}> */}
-      <Container fluid disableGutters maxWidth={false} sx={{ minHeight: '100vh', background: 'linear-gradient(135deg, #4B3B47 20%, #7E6377 40%, #00B2CA 60%, #7DCFB6 80%, #7DCFB6 100%)' }}>
+      <Container fluid disableGutters maxWidth={false} sx={{ minHeight: '100vh', background: 'linear-gradient(135deg, #4B3B47 20%, #7E6377 40%, #00B2CA 75%, #7DCFB6 100%)' }}>
         <Toolbar variant='regular' sx={{ position: 'sticky', zIndex: '15', backgroundColor: 'hsla(0, 100%, 0%, 0.8)' }}>
           <Image
             priority
             src="/images/headshot.jpg"
             className={utils.borderCircle}
-            height={60}
-            width={60}
+            height={50}
+            width={50}
             alt="profile-image"
           />
-          <div className="marginLeft: 10px">
-            <h2 className={utils.descriptionText}>Fiona Tang</h2>
+          <div>
+            <h2 className={utils.fullNameText}>Fiona Tang</h2>
           </div>
         </Toolbar>
-        <Grid container rowSpacing={{ xs: 2, md: 3 }} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid container rowSpacing={{ xs: 2, md: 3 }} columnSpacing={{ xs: 2, sm: 3, md: 5 }}>
           <Grid item xs={3} sx={{ maxHeight: '100vh', overflow: 'auto' }}>
             <Skills />
           </Grid>
-          <Grid item xs={9} sx={{ maxHeight: '100vh', overflow: 'auto' }}>
+          <Grid item xs={9} sx={{ maxHeight: '100vh', overflow: 'auto', paddingRight: '2.3rem' }}>
             <Overview />
             <Typography className={utils.headingMd}>Work Experience</Typography>
             <TMPCard />
@@ -57,21 +56,3 @@ export default function Resume() {
     </>
   );
 }
-
-
-    // <div>
-    //   {
-    //     ['left', 'right', 'top', 'bottom'].map((anchor) => (
-    //     <React.Fragment key={anchor}>
-    //       {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
-    //       {/* <Drawer
-    //         anchor={anchor}
-    //         open={state[anchor]}
-    //         onClose={toggleDrawer(anchor, false)}
-    //       >
-    //         {list(anchor)}
-    //       </Drawer> */}
-    //     </React.Fragment>
-    //   ))
-    //   }
-    // </div>
