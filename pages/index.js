79e17b3818408186f326f7603500e069c6afc919
fionatagious <ftang@codeforamerica.org>
2023-11-01 14:02:35 -0500

    px-16 at medium breakpoint

--- pages/index.js
+++ pages/index.js
@@ -14,22 +14,22 @@ export default function Home() {
       <Navbar />
       <Grid
         container
-        className="flex flex-col min-h-1/2 px-4 lg:px-32 pt-6 sm:pt-32"
+        className="flex flex-col min-h-1/2 px-4 xl:px-32 pt-6 sm:pt-32"
       >
         <Intro />
-        <Grid item xs={12} className="px-0 lg:px-32 py-20 my-20">
+        <Grid item xs={12} className="px-2 md:px-16 xl:px-32 py-20 my-20">
           <div className="heading text-slate-400 font-bold text-md text-center sm:text-left text-2xl sm:text-3xl m-4">
             About me
           </div>
           <About />
         </Grid>
-        <Grid item xs={12} className="px-0 lg:px-32 py-20 my-20">
+        <Grid item xs={12} className="px-2 md:px-16 xl:px-32 py-20 my-20">
           <div className="heading text-slate-400 font-bold text-md text-center sm:text-left text-2xl sm:text-3xl m-4">
             Experience
           </div>
           <ExperiencePanel />
         </Grid>
-        <Grid item xs={12} className="px-0 lg:px-32 pt-20 mt-20">
+        <Grid item xs={12} className="px-2 md:px-16 xl:px-32 pt-20 mt-20">
           <div className="heading text-slate-400 font-bold text-md text-center sm:text-left text-2xl sm:text-3xl m-4">
             Written Works
           </div>
