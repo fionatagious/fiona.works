9b04fd6c2984c4ea5fc1c264cf758ca6472ac313
Fiona Tang <ftang@codeforamerica.org>
2024-11-06 23:57:31 -0600

    update contact copy and email

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -5,16 +5,15 @@ import Grid from "@mui/material/Grid";
 export default function Contact() {
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
-      window.location = "mailto:fionatang@alum.berkeley.edu";
+      window.location = "mailto:fionatagious@gmail.com";
     }
   };
   return (
     <Grid container className="flex justify-center px-4">
       <Grid item xs={12} md={6} lg={5} className="grid content-start">
         <div className="text-center text-slate-950 text-sm sm:text-lg my-2">
-          If you want to collaborate on projects related to climate change,
-          civic tech, or data visualization / data journalism, I'd love to hear
-          from you.
+          I'm open to connecting with folks who are passionate about civic tech,
+          social impact, and sustainability.
         </div>
         <div
           className="flex justify-center mt-4 mb-2"
